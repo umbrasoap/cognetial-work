122ffe231e42b1c0f53ccd988f66ef9c94850c8b
Callan Percy <165254326+umbrasoap@users.noreply.github.com>
2024-03-28 09:18:44 +0200

    Create mainC.py

---
+++ mainC.py
@@ -0,0 +1,28 @@
+  def __init__(self, message, selected):
+        """Initialise color object with specified text and selected color."""
+        self._msg = message
+        self._color = COLORS[selected]
+
+    def __str__(self):
+        return self._color + self._msg + COLORS["end"]
+
+    def __add__(self, other):
+        return str(self) + other
+
+    def __radd__(self, other):
+        return other + str(self)
+
+def get_data_directory():
+    data_directory = os.getenv("TORBOT_DATA_DIR")
+    # if a path is not set, write data to the config directory
+    if not data_directory:
+        data_directory = project_root_directory
+
+    if data_directory.strip() == "":
+        data_directory = project_root_directory
+
+    # create directory if it doesn't exist
+    if not os.path.exists(data_directory):
+        os.mkdir(data_directory)
+
+    return data_directory
